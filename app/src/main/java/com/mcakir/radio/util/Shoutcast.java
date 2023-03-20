0279e3ffab0123c3462b825c6af21b67f8287901
Ezequiel Garcia <dinotoezequiel@gmail.com>
2023-03-20 20:33:02 -0300

    "radioDeBoca"

---
+++ app/src/main/java/com/mcakir/radio/util/Shoutcast.java
@@ -0,0 +1,36 @@
+package com.mcakir.radio.util;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Shoutcast {
+
+    private String name;
+
+    @SerializedName("stream")
+    private String url;
+
+    private String image;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return url;
+    }
+
+    public void setImage(String url) {
+        this.image = image;
+    }
+}
